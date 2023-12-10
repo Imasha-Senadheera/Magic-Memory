cb3479b145202c551da1ce671a14dce040f25cd2
Imasha Senadheera <imashasenadheera326@gmail.com>
2023-12-10 13:16:53 +0530

    2nd Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+
+const cardImages = [
+  { "src": "/img/bath.png" },
+  { "src": "/img/bee.png" },
+  { "src": "/img/bug.png" },
+  { "src": "/img/butter.png" },
+  { "src": "/img/golu.png" },
+  { "src": "/img/panu.png" },
+]
 
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  // shuffle cards for new game
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map(card => ({ ...card, id: Math.random() }))
+      
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
+  console.log(cards, turns)
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
 
-export default App;
+export default App
